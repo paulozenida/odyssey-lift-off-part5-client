eaae626bda95219efb970b9fef83c384df049040
Paulo Zenida <paulo.zenida@olx.com>
2022-02-03 18:17:22 +0000

    debug

--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ import GlobalStyles from './styles';
 import Pages from './pages';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
+console.info('all env properties', process.env);
+console.info('SERVER_URI', process.env.SERVER_URI);
+
 const client = new ApolloClient({
   uri: process.env.SERVER_URI || 'http://localhost:4000/',
   cache: new InMemoryCache(),
 });
 
 ReactDOM.render(
+
   <ApolloProvider client={client}>
     <GlobalStyles />
     <Pages />
